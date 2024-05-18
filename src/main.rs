64f80c555ac2e3ac0bd57e8e51fdaf2286ab0a4e
riz-j <adiputra979@gmail.com>
2024-05-18 21:00:45 +1000

    Move sse endpoints to different file for organization

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use async_std::channel;
 use async_std::sync::Mutex;
 use tide::sse;
 use tide::sse::Sender;
-use tide::Request;
 
 use crate::my_item::post_handler;
 
@@ -49,39 +48,11 @@ async fn main() -> tide::Result<()> {
 
     app.at("/").get(my_item::get_handler);
 
-    app.at("/sse-test")
-        .get(sse::endpoint(|_req, sender| async move {
-            sender.send("fruit", "banana", None).await?;
-            loop {
-                sender.send("fruit", "apple", None).await?;
-                async_std::task::sleep(std::time::Duration::from_secs(1)).await;
-            }
-        }));
+    app.at("/sse-test").get(sse::endpoint(my_sse::test_sse));
 
     app.at("/messages").post(post_handler);
 
-    app.at("/sse").get(sse::endpoint(sse_endpoint));
-    async fn sse_endpoint(req: Request<AppState>, sender: Sender) -> tide::Result<()> {
-        {
-            sender.send("message", "Connected!", None).await?;
-            // When client connects, append the sender to the AppState's client vector
-            let mut clients = req.state().clients.lock().await;
-            clients.push(sender);
-            println!("A client just connected!");
-        }
-
-        // Send messages to all clients when received
-        loop {
-            let message = req.state().receiver.recv().await.unwrap();
-            let clients = req.state().clients.lock().await;
-
-            for client in clients.iter() {
-                let _ = client.send("message", &message, None).await;
-            }
-
-            println!("Received message: {}", message);
-        }
-    }
+    app.at("/sse").get(sse::endpoint(my_sse::sse_endpoint));
 
     println!("App listening on port 8543");
     app.listen("127.0.0.1:8543").await?;
--- src/my_sse.rs
+++ src/my_sse.rs
@@ -1,1 +1,40 @@
+use tide::sse::Sender;
+use tide::Request;
 
+use crate::AppState;
+
+pub async fn sse_endpoint(req: Request<AppState>, sender: Sender) -> tide::Result<()> {
+    {
+        sender.send("message", "Connected!", None).await?;
+        // When client connects, append the sender to the AppState's client vector
+        let mut clients = req.state().clients.lock().await;
+        clients.push(sender);
+        println!("A client just connected!");
+    }
+
+    // Send messages to all clients when received
+    loop {
+        let message = req.state().receiver.recv().await.unwrap();
+        let clients = req.state().clients.lock().await;
+
+        for client in clients.iter() {
+            let _ = client.send("message", &message, None).await;
+        }
+
+        println!("Received message: {}", message);
+    }
+}
+
+// async_std::task::spawn(async move {
+//     while let Ok(message) = receiver.recv().await {
+//         println!("Received: {}", message);
+//     }
+// });
+
+pub async fn test_sse(_req: Request<AppState>, sender: Sender) -> tide::Result<()> {
+    sender.send("fruit", "banana", None).await?;
+    loop {
+        sender.send("fruit", "apple", None).await?;
+        async_std::task::sleep(std::time::Duration::from_secs(1)).await;
+    }
+}
