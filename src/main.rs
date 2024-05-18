5f8e095ec2e23226fd167f7dcf22f9f996c0068d
riz-j <adiputra979@gmail.com>
2024-05-18 14:26:27 +1000

    SUCCESSFULLY SEND TO MULTIPLE CLIENTS AT ONCE OMG

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod my_sse;
 use std::sync::Arc;
 
 use async_std::channel;
+use async_std::sync::Mutex;
 use tide::sse;
 use tide::sse::Sender;
 use tide::Request;
@@ -15,6 +16,7 @@ pub struct AppState {
     pub name: String,
     pub sender: Arc<channel::Sender<String>>,
     pub receiver: Arc<channel::Receiver<String>>,
+    pub clients: Arc<Mutex<Vec<Sender>>>,
 }
 impl AppState {
     fn new(
@@ -26,6 +28,7 @@ impl AppState {
             name,
             sender,
             receiver,
+            clients: Arc::new(Mutex::new(Vec::new())),
         }
     }
 }
@@ -57,18 +60,52 @@ async fn main() -> tide::Result<()> {
 
     app.at("/messages").post(post_handler);
 
-    app.at("/sse").get(sse::endpoint(
-        |req: Request<AppState>, sender: Sender| async move {
-            sender.send("message", "banana", None).await?;
+    app.at("/sse").get(sse::endpoint(sse_endpoint));
 
-            while let Ok(message) = &req.state().receiver.as_ref().recv().await {
-                sender.send("message", message, None).await?;
-                println!("Received message: {}", message);
+    async fn sse_endpoint(req: Request<AppState>, sender: Sender) -> tide::Result<()> {
+        sender.send("message", "banana", None).await?;
+
+        let app_state = req.state().clone();
+        {
+            let mut clients = app_state.clients.lock().await;
+            clients.push(sender);
+        }
+
+        // Send messages to all clients when received
+        loop {
+            let message = req.state().receiver.recv().await.unwrap();
+            let clients = req.state().clients.lock().await;
+            for client in clients.iter() {
+                if let Err(err) = client.send("message", &message, None).await {
+                    println!("Error sending message: {}", err);
+                }
             }
+            println!("Received message: {}", message);
+        }
+    }
 
-            Ok(())
-        },
-    ));
+    // app.at("/sse2").get(sse::endpoint(
+    //     |req: Request<AppState>, sender: Sender| async move {
+    //         // let app_state = req.state().clone();
+    //         // {
+    //         //     let mut clients = app_state.clients.lock().unwrap();
+    //         //     clients.push(sender);
+    //         // }
+    //         sender.send("message", "banana", None).await?;
+
+    //         let clients = req.state().clients.lock().await;
+    //         while let Ok(message) = &req.state().receiver.as_ref().recv().await {
+    //             for s in clients.iter() {
+    //                 s.send("message", message, None).await?;
+    //             }
+
+    //             // sender.send("message", message, None).await?;
+    //             println!("Received message: {}", message);
+    //         }
+
+    //         Ok(())
+    //     },
+    // ));
 
     // async_std::task::spawn(async move {
     //     while let Ok(message) = receiver.recv().await {
